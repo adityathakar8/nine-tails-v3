ce39073c367cb47fd8e2c977f68289b507886836
Aditya Thakar <69141779+adityathakar8@users.noreply.github.com>
2021-12-05 15:24:43 +0530

    Add files via upload

---
+++ src/components/home.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+    return (
+        <div>
+
+            <div className="home">
+
+
+                <div className="info">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.{" "}
+                    </p>
+                    <Link className="button-class" to="/form">
+                        <button type="button" className="btn btn-info">
+                            Get Started
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
